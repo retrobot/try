472ed5c349f517cb2d087e0963a8aa468a5abd99
Admin <Kamil@pepsita.gateway.2wire.net>
2013-04-09 01:52:54 +0100

    Initial files addition
    different typos

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<style>
+		.main {
+			}
+		.bloxor {
+			float: right;
+			height: 300px;
+
+		}
+
+	</style>
+	<title>Hi</title>
+	<p class="whats">hello every one on this</p>
+	<h1>lets go</h1>
+	<div class="main">try</div>
+
+</head>
+<body>
+	hi kk ll larry
+	<?php echo 'whats on <br/>'; ?>
+</body>
+</html>
